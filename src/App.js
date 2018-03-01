5f214480b2f563c23cfbc8683a54b7ec94b1ea8f
lauren.madigan <lauren.madigan@workday.com>
2018-03-01 09:21:31 -0800

    firebase auth init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Header, Button } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyB8yAxx0c4x1Kta2xuDQMNmdMwbfyWNa2I",
+      authDomain: "auth-f08e7.firebaseapp.com",
+      databaseURL: "https://auth-f08e7.firebaseio.com",
+      projectId: "auth-f08e7",
+      storageBucket: "auth-f08e7.appspot.com",
+      messagingSenderId: "1072149456694"
+    })
+  }
+
+
   render() {
     return (
       <View>
-        <Text>An App</Text>
+        <Header headerText="Authentication" />
+        <LoginForm/>
       </View>
     );
   }
